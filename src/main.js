40197e9b28a0e40b6bcfda309a4bae1fef9b7c8c
Dalibor Janák <dalibor.janak@gmail.com>
2015-06-23 13:26:48 +0200

    Use the Nominatim API (fixes #3)

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ goog.require('goog.debug.Logger');
 goog.require('goog.dom');
 goog.require('goog.events.EventType');
 // goog.require('goog.ui.AutoComplete.OSGaz');
-goog.require('goog.ui.ac.AutoComplete.GoogleGeocoder');
+goog.require('kt.Nominatim');
 
 goog.require('countyparish');
 
@@ -42,11 +42,11 @@ nlsgaz = function( callback ) {
   var parish = goog.dom.getElement('parish');
   var parish_span = goog.dom.getElement('parish_span');
   parish_span.style.display = 'none';
-  
+
   goog.dom.appendChild(county,
     goog.dom.createDom('option', { 'value':'' },
-    goog.dom.createTextNode('Choose...')
-  ));
+          goog.dom.createTextNode('Choose...')
+                  ));
 
   var last = "";
   goog.array.forEach(countyparish, function(r) {
@@ -54,8 +54,8 @@ nlsgaz = function( callback ) {
     last = r[0];
     var option = goog.dom.createDom('option', {
       'value':last },
-      goog.dom.createTextNode(last)
-    );
+    goog.dom.createTextNode(last)
+            );
     goog.dom.appendChild(county, option);
   });
 
@@ -64,77 +64,49 @@ nlsgaz = function( callback ) {
   goog.events.listen(county, goog.events.EventType.CHANGE, function(e) {
 		if (e.target.value == '') return parish_span.style.display = 'none'; 
 		else parish_span.style.display = 'inline';
-		goog.dom.removeChildren(parish);
-		// Add "Choose..."
-		goog.dom.appendChild(parish,
+    goog.dom.removeChildren(parish);
+    // Add "Choose..."
+    goog.dom.appendChild(parish,
 		  goog.dom.createDom('option', { 'value':'' },
-		  goog.dom.createTextNode('Choose...')
-		));
-		// Add all the values...
-		bbox = BBOX_MAX;
-		goog.array.forEach(countyparish, function(r) {
-			if (r[0] == e.target.value) {
-				bbox = [
-					Math.min(bbox[0], r[2]),
-					Math.min(bbox[1], r[3]),
-					Math.max(bbox[2], r[4]),
+            goog.dom.createTextNode('Choose...')
+                    ));
+    // Add all the values...
+    bbox = BBOX_MAX;
+    goog.array.forEach(countyparish, function(r) {
+      if (r[0] == e.target.value) {
+        bbox = [
+          Math.min(bbox[0], r[2]),
+          Math.min(bbox[1], r[3]),
+          Math.max(bbox[2], r[4]),
 					Math.max(bbox[3], r[5]) ];
-				var option = goog.dom.createDom('option', {
+        var option = goog.dom.createDom('option', {
 				  'value': [r[2],r[3],r[4],r[5]] },
-				  goog.dom.createTextNode(r[1])
-				);
-				goog.dom.appendChild(parish, option);
-			}
-		});
-		// Call the 'callback' function on the selected county bbox
-		callback(bbox[0], bbox[1], bbox[2], bbox[3]);
+        goog.dom.createTextNode(r[1])
+                );
+        goog.dom.appendChild(parish, option);
+      }
+    });
+    // Call the 'callback' function on the selected county bbox
+    callback(bbox[0], bbox[1], bbox[2], bbox[3]);
   });
 
   // Listener for "parish" select box
   goog.events.listen(parish, goog.events.EventType.CHANGE, function(e) {
 		if (e.target.value == '') return callback(bbox[0], bbox[1], bbox[2], bbox[3]);
-		var b = goog.array.map(e.target.value.split(','), parseFloat);
-		// Call the 'callback' function on the selected parish bbox
-		callback(b[0], b[1], b[2], b[3]);
+    var b = goog.array.map(e.target.value.split(','), parseFloat);
+    // Call the 'callback' function on the selected parish bbox
+    callback(b[0], b[1], b[2], b[3]);
   });
 
 
-  // Initialize gazeteer search
+  // Initialize Nominatim search
   var input = goog.dom.getElement('nlsgaz');
-  var ac = new goog.ui.ac.AutoComplete.GoogleGeocoder(input);
-
-goog.events.listen(ac, goog.ui.ac.AutoComplete.EventType.UPDATE, function(e) {
-    var bounds = e.row['geometry']['viewport'] // ['bounds'];
-    // logger.info('SELECT: ' + bounds);
-    callback(bounds.getSouthWest().lng(),
-		bounds.getSouthWest().lat(),
-		bounds.getNorthEast().lng(),
-		bounds.getNorthEast().lat());
+  if (input !== null) {
+    var ac = new kt.Nominatim(input);
+  }
+  
+  ac.registerCallback(function(bnds) {
+    callback(bnds[0], bnds[1], bnds[2], bnds[3]);
   });
-
-  var ngrid = /^([A-Z]{2})([0-9]+)$/i;
-  goog.events.listen(goog.dom.getElement('nlsgazform'),
-      goog.events.EventType.SUBMIT, function(e) {
-        e.preventDefault();
-        // logger.info('DIRECT: ' + input.value);
-        // test for the National Grid Reference
-        var m = input.value.replace(/\s/g,'').match(ngrid);
-        if (m !== null) {
-	        // logger.info('NGRID: ' + m[1].toUpperCase()+m[2]);
-	        callback(m[1].toUpperCase()+m[2]);
-		} else 
-	        ac.search(input.value, 1, function(token, result) {
-	          if (result.length > 0) {
-		          var bounds = result[0]['geometry']['viewport']; // ['bounds'];
-	            // logger.info(bounds);
-	            callback(bounds.getSouthWest().lng(),
-					bounds.getSouthWest().lat(),
-					bounds.getNorthEast().lng(),
-					bounds.getNorthEast().lat());
-	          }
-	        });
-      });
-
 };
-
 window['nlsgaz'] = nlsgaz;
